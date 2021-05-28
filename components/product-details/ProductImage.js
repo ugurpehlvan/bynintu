1e000ceb5c0c1d72c8c3b6f4c98f57d6fef259e5
Ugur <hireugurpehlivan@gmail.com>
2021-05-28 16:42:33 +0300

    Fix react-carousel

--- components/product-details/ProductImage.js
+++ components/product-details/ProductImage.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Slider from 'react-slick';
 import { Carousel } from 'react-responsive-carousel';
 
 class ProductImage extends Component {
@@ -18,46 +17,32 @@ class ProductImage extends Component {
     });
   }
 
-  // renderSliderMainImages = () => {
-  //   return (
-  //     this.props.product &&
-
-  //   );
-  // };
-
-  renderSliderSubImages = () => {
-    return (
-      this.props.product &&
-      this.props?.product?.imageUrls?.map((data, index) => {
-        return (
-          <div key={`sub-images-${index}`}>
-            <div className='item'>
-              <img src={data.url} alt='image' />
-            </div>
-          </div>
-        );
-      })
-    );
-  };
-
   render() {
     const { product } = this.props;
     return (
       <div className='col-lg-6 col-md-6'>
         <div className='products-page-gallery'>
           <div className='product-page-gallery-main'>
-            <div>
-              <Carousel showThumbs={true} showArrows={true}>
-                {product?.imageUrls?.map((data, index) => {
-                  return (
-                    <div key={`main-images${index}`}>
-                      <img alt='' src={data.url} />
-                    </div>
-                  );
-                })}
-              </Carousel>
-              <div style={{ height: '90px' }}></div>
-            </div>
+            <Carousel swipeable={true} thumbWidth={110} showArrows={true} showIndicators={false}>
+              {product?.imageUrls?.map((data, index) => {
+                return (
+                  <div
+                    style={{
+                      height: '600px',
+                      width: '396px',
+                      margin: 'auto',
+                      justifyContent: 'center',
+                      display: 'flex',
+                      alignItems: 'center',
+                    }}
+                    key={`main-images${index}`}
+                  >
+                    <img alt='' src={data.url} />
+                  </div>
+                );
+              })}
+            </Carousel>
+            <div style={{ height: '90px' }}></div>
           </div>
         </div>
       </div>
